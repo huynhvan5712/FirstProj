024300bbe8fc7480794388ae7e110f9ede931fab
huynhvan5712 <43528627+huynhvan5712@users.noreply.github.com>
2018-10-15 00:30:00 +0700

    Update HelloWorld.java

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,6 +1,9 @@
+import java.util.*;
 
 public class HelloWorld {
-	public void main (String[] args) {
-		System.out.print("Hello World I am Van");
+	public static void main (String[] args) {
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Enter your name: ");
+		System.out.print("Hello World I am " + sc.nextLine());
 	}
 }
